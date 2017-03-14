140ffab763943f8fddabebf9a8e7ccd2c2df34db
Scott Brenden <scott.b.brenden@gmail.com>
2017-03-14 15:23:18 -0700

    added in three project objects

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
-function Project(){};
+function Project(name, link){};
 
-var getfit = new Project();
+var getfit = new Project('Get Fit', 'https://aesthetiques.github.io/swole_devs/');
+var busMall = new Project('Bus Mall', 'https://scottbrenden.github.io/bus-mall/');
+var salmonCookies = new Project('Salmon Cookies', 'https://scottbrenden.github.io/Salmon-cookies/');
