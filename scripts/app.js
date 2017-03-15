f5b109aa01927e8e68893601696ffb2439a51f1c
Scott Brenden <scott.b.brenden@gmail.com>
2017-03-15 10:47:40 -0700

    added template for projects into html, and jQ function to creat the articles for each object

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 'use strict';
+var projects = [];
 
 function Project(name, link){
   this.name = name,
@@ -8,3 +9,17 @@ function Project(name, link){
 var getfit = new Project('Get Fit', 'https://aesthetiques.github.io/swole_devs/');
 var busMall = new Project('Bus Mall', 'https://scottbrenden.github.io/bus-mall/');
 var salmonCookies = new Project('Salmon Cookies', 'https://scottbrenden.github.io/Salmon-cookies/');
+
+var projectObjects = [getfit, busMall, salmonCookies];
+
+Project.prototype.toHtml = function () {
+  var $newProject = $('article.project-template').clone();
+  $newProject.removeClass();
+
+  $newProject.find('a').attr('href', this.ref);
+  $newProject.find('a').text(this.name);
+};
+
+projectObjects.forEach(function(proj) {
+  projects.push(new Project(proj));
+});
