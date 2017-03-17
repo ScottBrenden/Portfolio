43c1f6a153c89ba8d33adf9bf5bcbb233a92a842
Scott Brenden <scott.b.brenden@gmail.com>
2017-03-16 19:48:19 -0700

    adding nav functionality

--- scripts/navFunc.js
+++ scripts/navFunc.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var navBar = {};
+
+navBar.handleMainNav = function() {
+  $('nav').on('click', '.nav-item', function(){
+    $('section').hide();
+    $('#about').on('click', function(){
+      $('#about').show();
+    });
+    $('#projects').on('click', function(){
+      $('#projects').show();
+    });
+    $('#work-hist').on('click', function(){
+      $('#work-hist').show();
+    });
+  });
+};
