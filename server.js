6454b3691a02b9708e7db4249eaa905979055cc5
Scott Brenden <scott.b.brenden@gmail.com>
2017-03-24 11:12:10 -0700

    node server working

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('.'));
 
 app.get('*', function(request, response){
-  response.sendFile('index.html', {root: '.'});
+  response.sendFile('/index.html', {root: '.'});
 });
 
 app.listen(PORT, function(){
