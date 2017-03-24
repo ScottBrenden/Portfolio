d83bf0d1c342249f00e9b2223567afefd1dfc395
Scott Brenden <scott.b.brenden@gmail.com>
2017-03-24 11:02:28 -0700

    server.js set up

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('.'));
+
+app.get('*', function(request, response){
+  response.sendFile('index.html', {root: '.'});
+});
+
+app.listen(PORT, function(){
+  console.log(`on PORT localhost:${PORT}`);
+});
