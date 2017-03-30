122b104ec5d71026fbd29bb7357d68d5f360c3c4
Scott Brenden <scott.b.brenden@gmail.com>
2017-03-30 08:51:06 -0700

    changes

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,35 +1,37 @@
 'use strict';
 // var projects = [];
+(function(module){
+  function Project(projData){
+    this.name = projData.name,
+    this.ref = projData.ref;
+  };
 
-function Project(projData){
-  this.name = projData.name,
-  this.ref = projData.ref;
-};
+  Project.all = [];
 
-Project.all = [];
+  Project.prototype.toHtml = function () {
+    let source = $('#project-template').text();
+    let templateRender = Handlebars.compile(source);
 
-Project.prototype.toHtml = function () {
-  let source = $('#project-template').text();
-  let templateRender = Handlebars.compile(source);
+    return templateRender(this);
+  };
 
-  return templateRender(this);
-};
-
-Project.loadAll = function(rawData) {
-  rawData.forEach(function(element){
-    Project.all.push(new Project(element));
-  })
-};
-
-Project.fetchAll = function(){
-  if (localStorage.rawData) {
-    Project.loadAll(JSON.parse(localStorage.rawData));
-    projectView.initIndexPage();
-  } else {
-    $.getJSON('data/projectData.json', function(data){
-      Project.loadAll(data);
-      localStorage.setItem('rawData', JSON.stringify(Project.all));
-      projectView.initIndexPage();
+  Project.loadAll = function(rawData) {
+    rawData.forEach(function(element){
+      Project.all.push(new Project(element));
     })
+  };
+
+  Project.fetchAll = function(){
+    if (localStorage.rawData) {
+      Project.loadAll(JSON.parse(localStorage.rawData));
+      projectView.initIndexPage();
+    } else {
+      $.getJSON('data/projectData.json', function(data){
+        Project.loadAll(data);
+        localStorage.setItem('rawData', JSON.stringify(Project.all));
+        projectView.initIndexPage();
+      })
+    }
   }
-}
+  module.Project = Project;
+})(window);
